907b9fcff31406d706f73f845212fb704ef34234
Edna Sung <esung@lcs.on.ca>
2024-05-23 11:57:28 -0400

    Adjusted the view model so that newly added to-do items are persisted.

--- TodoList/Helpers/Supabase.swift
+++ TodoList/Helpers/Supabase.swift
@@ -10,5 +10,5 @@ import Supabase
 
 let supabase = SupabaseClient(
   supabaseURL: URL(string: "https://wjmidlvcbxsajfzihfbu.supabase.co")!,
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqbWlkbHZjYnhzYWpmemloZmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyOTY3NDgsImV4cCI6MjAzMTg3Mjc0OH0.TIR6SuOHBiVs_4ucfd-HzVCYljkrE3a96iFiNw05yycY"
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqbWlkbHZjYnhzYWpmemloZmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyOTY3NDgsImV4cCI6MjAzMTg3Mjc0OH0.TIR6SuOHBiVs_4ucfd-HzVCYljkrE3a96iFiNw05yyc"
 )
